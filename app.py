c193c87244b303f297049baf1f56aaf3b12ca092
shinitha-shine <128571462+shinitha-shine@users.noreply.github.com>
2023-06-30 13:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "<h1><center> TESTING THE VERSION1<center><h1>"
+  return "<h1><center> TESTING THE VERSION2<center><h1>"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=5000)
